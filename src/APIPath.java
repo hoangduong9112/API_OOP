1942829a0c3ac5f2c911f2176d2214c04295c400
hoangduong9112 <duonghoang9112@gmail.com>
2022-05-23 23:55:59 +0700

    add: signupAPI

--- src/APIPath.java
+++ src/APIPath.java
@@ -1,3 +1,4 @@
 public class APIPath {
     public static final String LOGIN = "https://auctions-app-2.herokuapp.com/api/login";
+    public static final String SIGNUP = "https://auctions-app-2.herokuapp.com/api/signup";
 }
