7eea45e1248d5b02d8e3711dd1c5ebe23cda4fa7
hoangduong9112 <duonghoang9112@gmail.com>
2022-05-24 10:58:43 +0700

    fix: add test ase

--- src/SignUpAPI.java
+++ src/SignUpAPI.java
@@ -71,9 +71,47 @@ public class SignUpAPI {
             IOException {
         List<TestCase<SignUpParams>> listTestCase = new ArrayList<>();
 
-        SignUpParams params1 = new SignUpParams("email", "duongh1oang@gmail.com", "password", "123456", "re_pass", "123456", "address", "hanoi", "name", "duong", "phone", "09123");
+        //Need to update new email to run test
+
+        SignUpParams params1 = new SignUpParams("email", "duonghoang120@gmail.com", "password", "123456", "re_pass", "123456", "address", "hanoi", "name", "duong", "phone", "09123");
         TestCase<SignUpParams> testCase1 = new TestCase<>("1000", "OK", "Unit test 1: Should be successful with correct param", params1);
         listTestCase.add(testCase1);
+
+        SignUpParams params2 = new SignUpParams("email", "duonghoang123@gmail.com", "password", "123456", "re_pass", "123456", "address", "hanoi", "name", "", "phone", "09123");
+        TestCase<SignUpParams> testCase2 = new TestCase<>("1001", "", "Unit test 2: Should throw error 1001 with empty name", params2);
+        listTestCase.add(testCase2);
+
+        SignUpParams params3 = new SignUpParams("email", "duonghoang124@gmail.com", "password", "123456", "re_pass", "123456", "address", "hanoi", "name", "duong", "phone", "");
+        TestCase<SignUpParams> testCase3 = new TestCase<>("1001", "", "Unit test 3: Should throw error 1001 with empty phone", params3);
+        listTestCase.add(testCase3);
+
+        SignUpParams params4 = new SignUpParams("email", "duonghoang103@gmail.com", "password", "123456", "re_pass", "123456", "address", "", "name", "duong", "phone", "09123");
+        TestCase<SignUpParams> testCase4 = new TestCase<>("1001", "", "Unit test 4: Should throw error 1001 with empty address", params4);
+        listTestCase.add(testCase4);
+
+        SignUpParams params5 = new SignUpParams("email", "", "password", "123456", "re_pass", "123456", "address", "hanoi", "name", "duong", "phone", "09123");
+        TestCase<SignUpParams> testCase5 = new TestCase<>("1001", "", "Unit test 5: Should throw error 1001 with empty email", params5);
+        listTestCase.add(testCase5);
+
+        SignUpParams params6 = new SignUpParams("email", "duonghoang125@gmail.com", "password", "", "re_pass", "123456", "address", "hanoi", "name", "duong", "phone", "09123");
+        TestCase<SignUpParams> testCase6 = new TestCase<>("1001", "", "Unit test 6: Should throw error 1001 with empty password", params6);
+        listTestCase.add(testCase6);
+
+        SignUpParams params7 = new SignUpParams("email", "123", "password", "123456", "re_pass", "123456", "address", "hanoi", "name", "duong", "phone", "09123");
+        TestCase<SignUpParams> testCase7 = new TestCase<>("1001", "", "Unit test 7: Should throw error 1001 with incorrect format email", params7);
+        listTestCase.add(testCase7);
+
+        SignUpParams params8 = new SignUpParams("email", "duonghoang100@gmail.com", "password", "123456", "re_pass", "123456", "address", "hanoi", "name", "duong", "phone", "09123");
+        TestCase<SignUpParams> testCase8 = new TestCase<>("1001", "", "Unit test 8: Should throw error 1001 with duplicated email", params8);
+        listTestCase.add(testCase8);
+
+        SignUpParams params9 = new SignUpParams("email", "duonghoang14@gmail.com", "password", "123456", "re_pass", "", "address", "hanoi", "name", "duong", "phone", "09123");
+        TestCase<SignUpParams> testCase9 = new TestCase<>("1001", "", "Unit test 9: Should throw error 1001 with empty repass", params9);
+        listTestCase.add(testCase9);
+
+        SignUpParams params10 = new SignUpParams("email", "duonghoang15@gmail.com", "password", "123456", "re_pass", "123", "address", "hanoi", "name", "duong", "phone", "09123");
+        TestCase<SignUpParams> testCase10 = new TestCase<>("1001", "", "Unit test 9: Should throw error 1001 with wrong repass", params10);
+        listTestCase.add(testCase10);
 
 
         System.out.println(ColorTerminal.ANSI_BLUE + "Testing SignUp API" + ColorTerminal.ANSI_RESET);
@@ -96,7 +134,8 @@ public class SignUpAPI {
         String value5;
         String key6;
         String value6;
-        private SignUpParams(String key1, String value1, String key2, String value2, String key3, String value3,String key4, String value4,String key5, String value5,String key6, String value6) {
+
+        private SignUpParams(String key1, String value1, String key2, String value2, String key3, String value3, String key4, String value4, String key5, String value5, String key6, String value6) {
             this.key1 = key1;
             this.value1 = value1;
             this.key2 = key2;
