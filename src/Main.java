c4c6df7d486adb02b83a40d57aabf1521a99a29f
hoangduong9112 <duonghoang9112@gmail.com>
2022-05-24 13:01:48 +0700

    fix: don't run all API

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-//            LoginAPI.main();
-            SignUpAPI.main();
+            LoginAPI.main();
+//            SignUpAPI.main();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
