d550939b9cfe14c46817184658160fc7d7a8e467
hoangduong9112 <duonghoang9112@gmail.com>
2022-05-25 20:22:39 +0700

    fix: issue

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import TestAPI.*;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            LoginAPI.main();
-//            SignUpAPI.main();
-//            EditAccountAPI.main();
+            TestLoginAPI.main();
+//            TestSignUpAPI.main(); // need update test case
+//            TestEditAccountAPI.main(); // need update test case
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
