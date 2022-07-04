5ba647f6f50f4a49d470fa5327576c2e9ba9f7bc
QuanDN22 <dinhngocquan112378@gmail.com>
2022-07-04 19:06:19 +0700

    fix TestSignUpAPI

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            TestLoginAPI.main();
+//            TestLoginAPI.main();
 
 //            TestGetDetailAuctionAPI.main();
 //            TestGetListAuctionsByTypeAPI.main();
 //            TestGetListComments.main();
 //            TestGetListAuctionsAPI.main();
-//            TestSignUpAPI.main(); // need update test case
+            TestSignUpAPI.main(); // need update test case
 //            TestEditAccountAPI.main(); // need update test case
 //            TestCreateBidAPI.main();
 //            TestCreateCommentAPI.main(); // need update test case
