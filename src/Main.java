a3bc38c91245450991bc159a861f942528610cce
hoangduong9112 <duonghoang9112@gmail.com>
2022-07-05 07:54:34 +0700

    fix: issue

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,24 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-//            TestLoginAPI.main();
-//            TestSignUpAPI.main(); // need update test case
-//            TestCreateAuctionAPI.main(); // need update test case
-//            TestGetListBids.main();
-//            TestCreateBidAPI.main(); // need update test case
-//            TestCreateCommentAPI.main();
-//            TestGetListComments.main();
-//            TestGetListAuctionsAPI.main();
-//            TestGetDetailAuctionAPI.main();
+            TestLoginAPI.main();
+            TestSignUpAPI.main(); // need update test case
+            TestCreateAuctionAPI.main(); // need update test case
+            TestGetListBids.main();
+            TestCreateBidAPI.main(); // need update test case
+            TestCreateCommentAPI.main();
+            TestGetListComments.main();
+            TestGetListAuctionsAPI.main();
+            TestGetDetailAuctionAPI.main();
             TestReadNewsAPI.main();
-//            TestGetSlider.main();
-//            TestSearch.main();
-//            TestTotalLikesOfAuction.main();
-//            TestGetNews.main();
-
-
-//            TestCreateItemAPI.main(); // need update test case
-//            TestEditAccountAPI.main(); // need update test case
-//            TestGetListLikesAPI.main();
-//            TestGetListAuctionsByTypeAPI.main();
+            TestGetSlider.main();
+            TestSearch.main();
+            TestTotalLikesOfAuction.main();
+            TestGetNews.main();
+            TestCreateItemAPI.main(); // need update test case
+            TestEditAccountAPI.main(); // need update test case
+            TestGetListLikesAPI.main();
+            TestGetListAuctionsByTypeAPI.main();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
